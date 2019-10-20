7f7213c04962edfffdb4c3378e7bf8707a41e978
wfgmiami <wfgmiami@yahoo.com>
2019-10-20 10:26:46 -0400

    wip

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -22,7 +27,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -30,7 +40,14 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Zip Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -38,7 +55,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -46,7 +68,12 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -56,20 +83,26 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'},
           ]
         },
-        value: ''
+        value: 'cheapest',
+        validation: {},
+        valid: true
       },
     },
+    formIsValid: false,
     loading: false
   }
 
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({ loading: true })
-
+    const formData = {};
+    for(let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-
+      orderData: formData
     }
     console.log('[ContactData] order', order)
     axios.post('/orders.json', order)
@@ -82,20 +115,45 @@ class ContactData extends Component {
       })
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
   inputChangedHandler = (name) => ({target: {value}}) => {
 
-    const updateOrderForm = {...this.state.orderForm};
+    const updatedOrderForm = {...this.state.orderForm};
 
-    const updatedFormElement = {...updateOrderForm[name]};
+    const updatedFormElement = {...updatedOrderForm[name]};
     updatedFormElement.value = value;
-    updateOrderForm[name] = updatedFormElement;
+    updatedFormElement.valid = this.checkValidity(value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
+    updatedOrderForm[name] = updatedFormElement;
 
-    this.setState({orderForm: updateOrderForm})
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({orderForm: updatedOrderForm, formIsValid})
   }
 
   render() {
     const formElementsArray = [];
-
+    console.log('[ContactData] this.state', this.state)
     for (let key in this.state.orderForm) {
       formElementsArray.push({
         id: key,
@@ -103,19 +161,21 @@ class ContactData extends Component {
 
       })
     }
-    console.log('this.state', this.state)
-    let form = (<form>
+    let form = (<form onSubmit={this.orderHandler}>
       {formElementsArray.map( formElement => (
         <Input
           key={formElement.id}
           elementType={formElement.config.elementType}
           elementConfig={formElement.config.elementConfig}
           value={formElement.config.value}
+          shouldValidate={formElement.config.validation}
+          invalid={!formElement.config.valid}
+          touched={formElement.config.touched}
           changed={this.inputChangedHandler(formElement.id)}
           />
       ))}
 
-      <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+      <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
     </form>);
 
     if (this.state.loading) {
